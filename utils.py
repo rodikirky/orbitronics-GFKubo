abbc45b24ffc81220d176745cad0d49de48c78da
rodikirky <leona.rodenkirchen@rwth-aachen.de>
2025-07-21 09:25:05 +0200

    Type hints

--- utils.py
+++ utils.py
@@ -1,8 +1,9 @@
 import numpy as np
 import sympy as sp
+from typing import Union
 
-
-def invert_matrix(matrix, symbolic):
+def invert_matrix(matrix: Union[np.ndarray, sp.Matrix],
+                  symbolic: bool) -> Union[np.ndarray, sp.Matrix]:
     """
     Helper function that inverts matrices in both numeric and symbolic mode.
     This function raises an error message, if the matrix is not invertible.
@@ -17,18 +18,20 @@ def invert_matrix(matrix, symbolic):
         return np.linalg.inv(matrix)
 
 
-def is_unitary(U, symbolic=False, atol=1e-10):
+def is_unitary(U: Union[np.ndarray, sp.Matrix],
+               symbolic: bool = False,
+               atol: float = 1e-10) -> bool:
     """
     This helper function checks, if a matrix is unitary.
     This needs to be verified for basis change operations.
     It operates in numeric mode. Hence, the matrix is first converted into a np.ndarray.
     """
-    U_eval = np.array(U.evalf()).astype(
-        np.complex128) if symbolic else np.array(U)
+    U_eval = np.array(U.evalf()).astype(np.complex128) if symbolic else np.array(U)
     return np.allclose(U_eval.conj().T @ U_eval, np.eye(U_eval.shape[0]), atol=atol)
 
 
-def sanitize_vector(vec, symbolic):
+def sanitize_vector(vec: Union[np.ndarray, list, tuple, sp.Matrix],
+                    symbolic: bool) -> Union[np.ndarray, sp.Matrix]:
     """
     This helper function ensures that the vector (vec) is a type fitting 
     for the mode we are working in, i.e. symbolic or numeric.
@@ -36,7 +39,8 @@ def sanitize_vector(vec, symbolic):
     return sp.Matrix(vec) if symbolic else np.array(vec, dtype=float)
 
 
-def get_identity(size, symbolic):
+def get_identity(size: int,
+                 symbolic: bool) -> Union[np.ndarray, sp.Matrix]:
     """
     This helper function returns the identity matrix needed for the 
     current mode: symbolic or numeric.
@@ -44,7 +48,8 @@ def get_identity(size, symbolic):
     return sp.eye(size) if symbolic else np.eye(size)
 
 
-def print_symbolic_matrix(matrix, name="Matrix"):
+def print_symbolic_matrix(matrix: sp.Matrix,
+                          name: str = "Matrix") -> None:
     """
     This helper function prints a symbolic matrix, which is 
     particularly helpful when debugging a symbolic Hamiltonian. 
