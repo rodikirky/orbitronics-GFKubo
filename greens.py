b3bd68deab4437a42d4aceb98ebc94df49115eb7
rodikirky <leona.rodenkirchen@rwth-aachen.de>
2025-07-22 09:53:03 +0200

    Added self.verbose and intermediate print calls for easy debugging and computation progress overview

--- greens.py
+++ greens.py
@@ -1,7 +1,7 @@
 import numpy as np
 import sympy as sp
 from typing import Callable, Union
-from utils import invert_matrix
+from utils import invert_matrix, print_symbolic_matrix
 
 
 class GreensFunctionCalculator:
@@ -9,9 +9,15 @@ class GreensFunctionCalculator:
                  hamiltonian: Callable[[Union[list, np.ndarray, sp.Matrix]], Union[np.ndarray, sp.Matrix]],
                  identity: Union[np.ndarray, sp.Matrix],
                  symbolic: bool,
+                 # omega
                  energy_level: Union[float, sp.Basic],
+                 # eta
                  infinitestimal: float,
-                 retarded: bool = True):
+                 # defaults to retarded Green's functions
+                 retarded: bool = True,
+                 # defaults to non-verbose
+                 # if verbose=True, intermediate steps will be printed out
+                 verbose: bool = False):
         """
         A calculator for Green's functions in momentum space.
 
@@ -22,13 +28,18 @@ class GreensFunctionCalculator:
         - energy_level: scalar ω
         - infinitestimal: small η > 0 to define the imaginary part
         - retarded: if True computes retarded Green's function; else advanced
+        - verbose: if True, prints intermediate matrix states for debugging
         """
+
         self.H = hamiltonian
         self.I = identity
         self.symbolic = symbolic
         self.omega = energy_level
         self.eta = infinitestimal
         self.q = 1 if retarded else -1
+
+        # for easy debugging along the way
+        self.verbose = verbose
 
     def compute_kspace_greens_function(self, momentum: Union[np.ndarray, sp.Matrix]) -> Union[np.ndarray, sp.Matrix]:
         """
@@ -40,11 +51,11 @@ class GreensFunctionCalculator:
         Returns:
         - Green's function matrix at momentum k
         """
-        H_k = self.H(momentum)  # hamiltonian in k-space
-        omega_I = self.omega * self.I  # allows for matric arithmetic with a scalar
-        i_eta = (sp.I if self.symbolic else 1j) * \
-            self.eta * self.I  # imaginary part
-        q = self.q  # retarded (q = 1) or advanced (q = -1)
+        H_k = self.H(momentum)  # Hamiltonian at k
+        omega_I = self.omega * self.I  # Frequency term scaled identity
+        i_eta = (sp.I if self.symbolic else 1j) * self.eta * self.I  # Imaginary part for broadening
+        q = self.q # q = 1 for retarded GF, q = -1 for advanced
+
         tobe_inverted = omega_I + q * i_eta - H_k
 
         if self.symbolic:
@@ -52,5 +63,10 @@ class GreensFunctionCalculator:
             tobe_inverted = sp.Matrix(tobe_inverted.as_explicit())
         else:
             tobe_inverted = np.array(tobe_inverted)
+
+        if self.verbose:
+            print("\nComputing Green's function at k:")
+            print_symbolic_matrix(H_k, name="H(k)") if self.symbolic else print("H(k) =\n", H_k)
+            print_symbolic_matrix(tobe_inverted, name="( ω ± iη - H(k) )") if self.symbolic else print("Inversion target =\n", tobe_inverted)
 
         return invert_matrix(tobe_inverted, symbolic=self.symbolic)
