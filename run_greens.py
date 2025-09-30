62645f013d9a1dcf325deee1fa6e6dd0a8969d8f
rodikirky <leona.rodenkirchen@rwth-aachen.de>
2025-09-30 08:30:22 +0200

    Added a runner

---
+++ run_greens.py
@@ -0,0 +1,5 @@
+import logging, warnings
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s %(levelname)s %(name)s: %(message)s")
+warnings.captureWarnings(True)  # optional
+
