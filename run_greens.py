e7792e838443ae617c98954513c15532d40414b6
rodikirky <leona.rodenkirchen@rwth-aachen.de>
2025-09-30 08:51:03 +0200

    Warnings routed to logging.

--- run_greens.py
+++ run_greens.py
@@ -1,5 +1,4 @@
-import logging, warnings
+import logging
 logging.basicConfig(level=logging.INFO,
                     format="%(asctime)s %(levelname)s %(name)s: %(message)s")
-warnings.captureWarnings(True)  # optional
-
+logging.captureWarnings(True) # optional: route warnings -> logging
